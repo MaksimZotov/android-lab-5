4597215381b998c617c06feae22bb93a691683ab
MaksimZotov <zotovmaksim1254@gmail.com>
2021-12-28 00:48:49 +0300

    in progress

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/task2/MainActivity2.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/task2/MainActivity2.kt
@@ -1,19 +1,28 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch.task2
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import ru.spbstu.icc.kspt.lab2.continuewatch.R
 import ru.spbstu.icc.kspt.lab2.continuewatch.getExecutorService
+import java.io.InterruptedIOException
 import java.net.URL
+import java.util.concurrent.Future
 
 
 class MainActivity2 : AppCompatActivity() {
     companion object {
+        private const val TAG = "MainActivity2"
         private const val URL = "https://open.spbstu.ru/wp-content/uploads/2018/09/04KOTLIN2.jpg"
     }
+
+    private var task: Future<*>? = null
+    private val iconBitmap: MutableLiveData<Bitmap?> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +31,33 @@ class MainActivity2 : AppCompatActivity() {
         val image = findViewById<ImageView>(R.id.iv_image)
         val download = findViewById<Button>(R.id.btn_download)
 
+        iconBitmap.observe(this) { icon ->
+            image.setImageBitmap(icon)
+        }
+
         download.setOnClickListener {
-            application.getExecutorService()?.execute {
-                val url = URL(URL)
-                val icon = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-                runOnUiThread { image.setImageBitmap(icon) }
+            task?.let {
+                if (!it.isDone) {
+                    it.cancel(true)
+                }
+            }
+            task = application.getExecutorService()?.submit {
+                try {
+                    Log.i(TAG, "Thread is launched")
+                    val url = URL(URL)
+                    val icon = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                    iconBitmap.postValue(icon)
+                    Log.i(TAG, "Task is done")
+                } catch (ex: InterruptedIOException) {
+                    Log.i(TAG, "Thread is interrupted")
+                    ex.printStackTrace()
+                }
             }
         }
+    }
+
+    override fun onDestroy() {
+        task?.cancel(true)
+        super.onDestroy()
     }
 }
--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/task3/MainActivity3.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/task3/MainActivity3.kt
@@ -1,22 +1,29 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch.task3
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import ru.spbstu.icc.kspt.lab2.continuewatch.R
+import ru.spbstu.icc.kspt.lab2.continuewatch.task2.MainActivity2
+import java.io.InterruptedIOException
 import java.net.URL
 
 
 class MainActivity3 : AppCompatActivity() {
     companion object {
+        private const val TAG = "MainActivity3"
         private const val URL = "https://open.spbstu.ru/wp-content/uploads/2018/09/04KOTLIN2.jpg"
     }
+
+    private var job: Job? = null
+    private val iconBitmap: MutableLiveData<Bitmap?> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +32,23 @@ class MainActivity3 : AppCompatActivity() {
         val image = findViewById<ImageView>(R.id.iv_image)
         val download = findViewById<Button>(R.id.btn_download)
 
+        iconBitmap.observe(this) { icon ->
+            image.setImageBitmap(icon)
+        }
+
         download.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO) {
-                val url = URL(URL)
-                val icon = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-                withContext(Dispatchers.Main) { image.setImageBitmap(icon) }
+            job?.cancel() // just for tests
+            job = lifecycleScope.launch(Dispatchers.IO) {
+                try {
+                    Log.i(TAG, "Coroutine is launched")
+                    val url = URL(URL)
+                    val icon = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                    iconBitmap.postValue(icon)
+                    Log.i(TAG, "Coroutine is done")
+                } catch (ex: Exception) {
+                    Log.i(TAG, "Coroutine is cancelled")
+                    ex.printStackTrace()
+                }
             }
         }
     }
