ad62ae84d811ec3baf75534b31b3048e6bace187
MaksimZotov <zotovmaksim1254@gmail.com>
2021-12-28 02:01:59 +0300

    in progress

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/task1/MainActivity1_1.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/task1/MainActivity1_1.kt
@@ -12,7 +12,7 @@ import java.util.*
 class MainActivity1_1 : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity1_1"
-        private const val MILLISECONDS_ELAPSED_KEY = "MILLISECONDS_KEY"
+        private const val MILLISECONDS_ELAPSED_KEY = "MILLISECONDS_KEY_"
     }
 
     private lateinit var sharedPreferences: SharedPreferences
@@ -23,17 +23,40 @@ class MainActivity1_1 : AppCompatActivity() {
 
     private lateinit var backgroundThread: Thread
     private val threadRunnable = Runnable {
+        var startTime = Date().time
         try {
             Log.i(TAG, "Thread is launched")
+            Log.i(TAG, "Seconds (first): $secondsElapsed")
+
+            textSecondsElapsed.post {
+                textSecondsElapsed.text = getString(R.string.seconds_elapsed, secondsElapsed)
+            }
+
+            val difMillisecondsAndNextSeconds =
+                if (millisecondsElapsed == 0L) 1000
+                else 1000 - (millisecondsElapsed - secondsElapsed * 1000)
+
+            Log.i(TAG, "DIF between milliseconds " +
+                    "and next seconds: $difMillisecondsAndNextSeconds")
+
+            Thread.sleep(difMillisecondsAndNextSeconds)
+
+            var nextDelay = 1000L
             while (true) {
-                Log.i(TAG, "Seconds: $secondsElapsed")
+                Log.i(TAG, "Seconds: ${++secondsElapsed}")
                 textSecondsElapsed.post {
-                    textSecondsElapsed.text = getString(R.string.seconds_elapsed, secondsElapsed++)
+                    textSecondsElapsed.text = getString(R.string.seconds_elapsed, secondsElapsed)
                 }
-                Thread.sleep(1000)
+                startTime = Date().time
+                Thread.sleep(nextDelay)
+                val endTime = Date().time
+                val correction = endTime - startTime - 1000
+                nextDelay = (1000 - correction)
             }
         } catch (ex: InterruptedException) {
+            millisecondsElapsed = (Date().time - startTime) + secondsElapsed * 1000
             Log.i(TAG, "Thread is interrupted")
+            Log.i(TAG, "Elapsed milliseconds: $millisecondsElapsed")
         }
     }
 
@@ -64,7 +87,6 @@ class MainActivity1_1 : AppCompatActivity() {
         super.onPause()
         Log.i(TAG, "onPause")
         backgroundThread.interrupt()
-        millisecondsElapsed += (Date().time - initMilliseconds)
     }
 
     override fun onStop() {
